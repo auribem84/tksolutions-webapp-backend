bba26d3ce0b0e23b13afee011847ad90869220cf
Arturo Uribe <auribe21@Mac.lan>
2026-05-02 21:44:14 -0400

    update CORS

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ origins = [
     "http://127.0.0.1:8081",
     "http://127.0.0.1:8082",
     "http://localhost:5173",
+    "https://tksolutions-webapp-frontend.onrender.com",
 ]
 
 app.add_middleware(
