b4ac5498596e5d0beae4c85b2d2bb8d3b271d8ce
Arturo Uribe <auribe21@Mac.lan>
2026-05-19 16:52:59 -0400

    invoice email2

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -10,8 +10,8 @@ import os
 ses = boto3.client(
     "ses",
     region_name=os.getenv("AWS_REGION"),
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
 )
 
 
